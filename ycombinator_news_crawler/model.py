15e5b043d995e673f51e9bea43c13fcaea5635cf
biboy1999 <biboy1999@gmail.com>
2022-05-10 03:02:47 +0800

    impl past ask show
    rename some attr in news model

--- ycombinator_news_crawler/model.py
+++ ycombinator_news_crawler/model.py
@@ -1,15 +1,22 @@
 class News:
-    def __init__(self, title, id, link, points, agoTime, commentsNum, user=""):
-        self.title = title
-        self.id = id
-        self.link = link
+    def __init__(self, news_title, news_id, news_link, points, username, post_time, comments_count):
+        self.news_title = news_title
+        self.news_id = news_id
+        self.news_link = news_link
         self.points = points
-        self.user = user
-        self.agoTime = agoTime
-        self.commentsNum = commentsNum
+        self.user = username
+        self.post_time = post_time
+        self.comments_count = comments_count
 
     def formatOutput(self):
-        return '{}\nlink: {}\npoints: {}\n{}\ncommentNum: {}\n'.format(self.title, self.link, self.points, self.agoTime, self.commentsNum)
+        # return '{}\nlink: {}\npoints: {}\n{}\ncommentNum: {}\n'.format(self.news_title, self.news_link, self.points, self.post_time, self.comments_count)
+        return f"""title:{self.news_title}
+link:{self.news_link}
+point:{self.points}
+user:{self.user}
+post time:{self.post_time}
+comments count:{self.comments_count}
+"""
 
     def __str__(self):
         return self.formatOutput()
--- ycombinator_news_crawler/news.py
+++ ycombinator_news_crawler/news.py
@@ -1,50 +1,70 @@
-from hashlib import new
-from .config import BASE_URL
-from .model import News
+from contextlib import nullcontext
+from ycombinator_news_crawler.config import BASE_URL
+from ycombinator_news_crawler.model import News
 from datetime import date
 from re import compile
 from requests import get
 from bs4 import BeautifulSoup
 
-NEWEST_URL = f"{BASE_URL}/newest"
 
-
-def get_newest(amount: int = 30) -> list[News]:
+def _get_news(amount: int, news_type: str) -> list[News]:
     result = []
+    next_page = news_type
+    while len(result) < amount and next_page is not None:
+        resp = get(f"{BASE_URL}/{next_page}")
+        body = BeautifulSoup(resp.text, "html.parser")
 
-    resp = get(NEWEST_URL)
-    body = BeautifulSoup(resp.text, "html.parser")
-    news_list = body.find_all(class_="athing")
-    for news in news_list:
-        news_title = news.find(class_="titlelink").text
-        news_id = news.attrs["id"]
-        news_link = news.find(class_="titlelink").attrs["href"]
+        next_page = body.find(class_="morelink")
+        # is None when last page otherwise next page link
+        next_page = next_page.attrs["href"] if next_page is not None else None
 
-        sub_info = news.next_sibling.find(class_="subtext")
-        point = int(sub_info.find(class_="score").text.split()[0])
-        age = sub_info.find(class_="age").attrs["title"]
-        comment_info = sub_info.find(class_=compile(r"^item\?id=\d+"))
-        commentNum = int(comment_info.split()[
-                         0]) if comment_info is not None else 0
+        news_list = body.find_all(class_="athing")
+        for news in news_list:
 
-        username = sub_info.find(class_="hnuser").text
+            if len(result) >= amount:
+                return result
 
-        result.append(News(news_title, news_id,
-                      news_link, point, age, commentNum, username))
+            news_title = news.find(class_="titlelink").text
+            news_id = news.attrs["id"]
+            news_link = news.find(class_="titlelink").attrs["href"]
+
+            # extract sub info
+            sub_info = news.next_sibling.find(class_="subtext")
+            point = int(sub_info.find(class_="score").text.split()[0])
+            age = sub_info.find(class_="age").attrs["title"]
+            # use \s here there is \xa0 non-breaking space in between
+            comment_info = sub_info.find(string=compile(r"\d+\scomment"))
+            comment_count = int(comment_info.split()[
+                0]) if comment_info is not None else 0
+
+            username = sub_info.find(class_="hnuser").text
+
+            result.append(News(news_title, news_id,
+                               news_link, point, username, age, comment_count))
 
     return result
 
 
-def get_news(amount: int = 30, search_date: str = None) -> list[News]:
+def get_newest(amount: int = 30) -> list[News]:
+    return _get_news(amount, "newest")
+
+
+def get_past(amount: int = 30, search_date: str = None) -> list[News]:
     if search_date is None:
         search_date = date.today().strftime("%Y-%m-%d")
 
-    raise NotImplementedError()
+    return _get_news(amount, f"front?day={search_date}")
 
 
 def get_ask(amount: int = 30) -> list[News]:
-    raise NotImplementedError()
+    return _get_news(amount, "ask")
 
 
 def get_show(amount: int = 30) -> list[News]:
-    raise NotImplementedError()
+    return _get_news(amount, "show")
+
+
+if __name__ == "__main__":
+    ls = get_past(100, "2022-05-07")
+    print(f"count: {len(ls)}, title: {str(ls[0])}")
+    pass
